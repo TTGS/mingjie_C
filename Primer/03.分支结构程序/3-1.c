1325cc301c942892d1f689450ff4e62c602858e3
hehe <foggyglass@163.com>
2020-02-02 11:58:54 +0800

    add 3-1.c

--- Primer/03.分支结构程序/3-1.c
+++ Primer/03.分支结构程序/3-1.c
@@ -2,7 +2,19 @@
 
 int main(void)
 {
+int x,y;
+printf("请输入两个整数。\n");
 
+printf("整数A：");
+scanf("%d",&x);
+
+printf("整数B：");
+scanf("%d",&y);
+
+if (x%y!=0)  
+       puts("B不是A的约数。");	
+else 
+       puts("B是A的约数。");	
 
 return 0;
 }
