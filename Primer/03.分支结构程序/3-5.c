d6aaee4f7064a7b2fbac251ba78ee16e92336933
hehe <foggyglass@163.com>
2020-02-04 19:29:10 +0800

    add 3-5.c

---
+++ Primer/03.分支结构程序/3-5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+//相等运算符
+if ((3!=2)==0)  
+       printf("3!=2 is 0\n");	
+else if ((3!=2)==1)  
+       printf("3!=2 is 1 \n");	
+
+if ((3==2)==0)  
+       printf("3==2 is 0\n");	
+else if ((3==2)==1)  
+       printf("3==2 is 1 \n");	
+
+//关系运算符
+if ((3>=2)==0)  
+       printf("3>=2 is 0\n");	
+else if ((3>=2)==1)  
+       printf("3>=2 is 1 \n");	
+
+if ((3>2)==0)  
+       printf("3> 2 is 0\n");	
+else if ((3>2)==1)  
+       printf("3> 2 is 1 \n");	
+
+if ((3<=2)==0)  
+       printf("3<=2 is 0\n");	
+else if ((3<=2)==1)  
+       printf("3<=2 is 1 \n");	
+
+if ((3<2)==0)  
+       printf("3< 2 is 0\n");	
+else if ((3<2)==1)  
+       printf("3< 2 is 1 \n");	
+
+return 0;
+}
