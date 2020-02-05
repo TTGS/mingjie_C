7c14eef4270345b41ea5ffac95719ee44e49d7a3
hehe <foggyglass@163.com>
2020-02-05 22:02:55 +0800

    add 4-9.c

---
+++ Primer/04.程序的循环控制/4-9.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int no ;
+
+	printf("正整数");
+	scanf("%d", &no);
+
+
+	while (no-- > 0)
+            if ((no%2)==1)
+		putchar('+');
+            else 
+                putchar('-');
+ 
+	putchar('\n');
+
+	return 0;
+}
