138b984320800fe0f3c844ff58088588e39278f1
hehe <foggyglass@163.com>
2020-02-05 20:11:46 +0800

    add 4-3.c

--- Primer/04.程序的循环控制/4-3.c
+++ Primer/04.程序的循环控制/4-3.c
@@ -2,7 +2,8 @@
 
 int main(void)
 {
-	int no,n;
+	int no,n; //额外引入n变量，如果不引入该变量，那么输入-1也会给予一个换行。
+
 
 	printf("请输入一个正整数：");
 	scanf("%d", &no);
@@ -11,6 +12,9 @@ int main(void)
 	while (no >= 0) {
 		printf("%d ", no );
 		no--;			/* no的值递减 */
+
+        //本想在这里写一个控制换行的，但这需要判断no次，影响效率就没有这样做。
+
         }
         if ((no==-1) && (n>=0) )
 	        printf("\n");		/* 换行 */
