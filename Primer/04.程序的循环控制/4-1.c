60ac1d7f47eda633c83c44cb7ad0246e97807be5
hehe <foggyglass@163.com>
2020-02-05 13:06:32 +0800

    add 4-1.c

--- Primer/04.程序的循环控制/4-1.c
+++ Primer/04.程序的循环控制/4-1.c
@@ -1,1 +1,27 @@
-1
+/*
+	判断输入的整数的符号
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	int no;
+	int i=1;
+	int retry;
+do {
+	printf("第%d次,请输入一个整数：",i);
+	scanf("%d", &no);
+
+	if (no == 0)
+		puts("该整数为0。");
+	else if (no > 0)
+		puts("该整数为正数。");
+	else
+		puts("该整数为负数。");
+i++;
+		printf("要重复一次吗？【Yes…0 / No…9】:");
+		scanf("%d", &retry);
+}while(retry==0)
+	return 0;
+}
