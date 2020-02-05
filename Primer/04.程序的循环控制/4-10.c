2d6ae5f51ce22b37ecbba0a1f17ef1769ff2552b
TT <foggyglass@163.com>
2020-02-05 22:09:06 +0800

    Create 4-10.c

---
+++ Primer/04.程序的循环控制/4-10.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int no ;
+
+	printf("正整数: ");
+	scanf("%d", &no);
+
+
+	while (no-- > 0){
+		putchar('*');
+                putchar('\n');
+ }
+	putchar('\n');
+
+	return 0;
+}
