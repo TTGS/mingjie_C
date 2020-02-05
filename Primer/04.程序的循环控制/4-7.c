db445c79a74bcc8420f5e88d9e49562cb17ca935
hehe <foggyglass@163.com>
2020-02-05 21:33:56 +0800

    add 4-7.c

--- Primer/04.程序的循环控制/4-7.c
+++ Primer/04.程序的循环控制/4-7.c
@@ -2,18 +2,15 @@
 
 int main(void)
 {
-	int i, no,p;
+	int i, no;
 
 	printf("请输入一个正整数：");
 	scanf("%d", &no);
 
 	i = 2;
-	p=1;
 	while (i <= no){
-	i=i^p;
+	i=i*2;
 	printf("%d ", i);		/* i的值在显示之后递增 */
-	p++;	
-
 	}
         printf("\n");
 	return 0;
