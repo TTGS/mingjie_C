af8195879b7b56e1e3194730b737a215de27b5a7
hehe <foggyglass@163.com>
2020-02-05 19:52:09 +0800

    add 4-2.c

---
+++ Primer/04.程序的循环控制/4-2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a ,b ;
+        int sum=0; 
+        int temp;
+//获得整数a，b
+	printf("请输入两个个整数：\n");
+	printf("整数 a ：");
+	scanf("%d", &a);
+
+	printf("整数 b ：");
+	scanf("%d", &b);
+        temp=a;
+
+do {
+sum=sum+temp;
+temp=temp-1
+}while(temp!=b);
+
+	printf("大于等于%d小于等于%d的所有整数的和是:%d \n",a,b,sum);
+	return 0;
+}
