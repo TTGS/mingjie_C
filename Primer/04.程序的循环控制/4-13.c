a2b4b558f9ac83eac47e8f2144d3fb959e0164d0
hehe <foggyglass@163.com>
2020-02-06 16:07:38 +0800

    add 4-13.c

---
+++ Primer/04.程序的循环控制/4-13.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int no,sum,i;
+
+	printf("请输入一个正整数：");
+	scanf("%d", &no);
+ 	i=no;
+	while (i>=0) { 
+		 sum=sum+i;
+		i--;
+	}
+	printf("1到%d的和为%d 。\n",no,sum); 
+
+	return 0;
+}
