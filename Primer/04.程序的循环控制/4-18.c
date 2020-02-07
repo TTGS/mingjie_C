958667f333076e4a44687e0bceeb54d37543503e
hehe <foggyglass@163.com>
2020-02-07 20:14:07 +0800

    add 4-18.c

---
+++ Primer/04.程序的循环控制/4-18.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n,i;
+
+	printf("显示多少个*：");
+	scanf("%d", &n);
+	i=1;
+
+	while (i<=n) { 
+		printf("*");
+                if (i==5)
+			printf("\n");
+ 		i++;
+	} 
+
+	return 0;
+}
