d93c72de93ff5a47125653fa5841884434de906c
hehe <foggyglass@163.com>
2020-02-07 20:32:01 +0800

    add 4-19.c

--- Primer/04.程序的循环控制/4-19.c
+++ Primer/04.程序的循环控制/4-19.c
@@ -2,16 +2,17 @@
 
 int main(void)
 {
-	int n,i;
+	int n,i,y;
 
 	printf("整数值：");
 	scanf("%d", &n);
 	i=1;
-
+        y=0;
 	while (i<=n) { 
 		
-                if ((n%i)==0)
+                if ((n%i)==0){
 			printf("%d\n",i);
+                        y=y+1;}
  		i++;
 	} 
 	printf("约数有%d个\n" ,i);
