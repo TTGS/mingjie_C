2097c87c0ded3124683f06797f558f462fb6a278
hehe <foggyglass@163.com>
2020-02-10 06:28:07 +0800

    add 4-24a.c

--- Primer/04.程序的循环控制/4-24a.c
+++ Primer/04.程序的循环控制/4-24a.c
@@ -15,7 +15,7 @@ int main(void)
 		for (i = 1; i <= len; i++)
 		{
 			for(j=1;j<= (2*len-1 );j++){
-				if((j< (2*i-i)) || (j> (2*i+i)   ) )
+				if( j<=len-i   )
 					putchar(' ');
 				else 
 					putchar('*'); 
