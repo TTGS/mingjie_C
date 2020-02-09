73dd4219a9e572f6001c8d7d737947ed9c7d9bbe
hehe <foggyglass@163.com>
2020-02-09 22:36:07 +0800

    add 4-24a.c

---
+++ Primer/04.程序的循环控制/4-24a.c
@@ -0,0 +1,28 @@
+/*
+	显示直角在左下方的等腰直角三角形
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	int i, j, len;
+
+	puts("让我们来画一个金字塔。");
+	printf("金字塔有几层：");
+	scanf("%d", &len);
+ 
+		for (i = 1; i <= len; i++)
+		{
+			for(j=1;j<= (2*len-1 );j++){
+				if((j<=(2*len-1) -i) || (j>=(2*len-1)+i   ) )
+					putchar(' ');
+				else 
+					putchar('*'); 
+			}
+
+		putchar('\n');	 
+		}
+
+	return 0;
+}
