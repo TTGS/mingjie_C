22c3093903e241428ba4016803958450845baf10
hehe <foggyglass@163.com>
2020-02-10 07:02:30 +0800

    add 4-25.c

---
+++ Primer/04.程序的循环控制/4-25.c
@@ -0,0 +1,24 @@
+
+
+#include <stdio.h>
+
+int main(void)
+{
+	int i, j, len;
+
+	puts("让我们来画一个向下的金字塔。");
+	printf("金字塔有几层：");
+	scanf("%d", &len);
+ 
+		for (i = 1; i <= len; i++)
+		{
+			for(j=1;j<= len+i-1 ;j++){
+				if( j<=len-i   )
+					putchar(' ');
+				else 
+					putchar('*'); 
+			}
+		putchar('\n');	 
+		}
+	return 0;
+}
