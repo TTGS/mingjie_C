e09d6f7233b0aee8e849aca4b7944100344a055e
hehe <foggyglass@163.com>
2020-02-09 16:47:29 +0800

    add 4-20b.c

--- Primer/04.程序的循环控制/4-20b.c
+++ Primer/04.程序的循环控制/4-20b.c
@@ -7,15 +7,16 @@
 int main(void)
 {
 	int i, j;
-
+printf(" |  1  2  3  4  5  6  7  8  9 \n");
+printf("---------------------------------\n");
 	for (i = 1; i <= 9; i++) {
 		for (j = 1; j <= 9; j++)
 			{
-if (j==1) 
-printf("%2d|", i * j);
-else 
-printf("%3d", i * j);
-}
+			if (j==1) 
+			printf("%2d|", i * j);
+			else 
+			printf("%3d", i * j);
+			}
 		putchar('\n');				/* 换行 */
 	}
 
