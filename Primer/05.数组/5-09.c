0675078e909dab183af655d2364f5c29d129b744
hehe <foggyglass@163.com>
2020-02-14 21:12:57 +0800

    add 5-09.c

--- Primer/05.数组/5-09.c
+++ Primer/05.数组/5-09.c
@@ -8,11 +8,11 @@
 
 int main(void)
 {
-	int i, j;
+	int i, j ;
 	int num;				/* 实际的人数 */
 	int tensu[NUMBER];		/* 学生的分数 */
 	int bunpu[11] = {0};	/* 分布图 */
-
+	int max ;
 	printf("请输入学生人数：");
 
 	do {
@@ -33,22 +33,22 @@ int main(void)
 		bunpu[tensu[i] / 10]++;
 	}
 
-	puts("\n---分布图---");
-
-
+//比大小
 	for (i =0; i <= 9; i++) {
-printf("%d : %d\n",i,bunpu[i]);				 
-		/*printf("%3d - %3d：", i * 10, i * 10 + 9);
-		for (j = 0; j < bunpu[i]; j++)
-			putchar('*');
-		putchar('\n');
-	}
-	printf("      100：");
-
-	for (j = 0; j < bunpu[10]; j++)			 
-		putchar('*');
-	putchar('\n'); */
-
+		max=bunpu[0];
+		if (bunpu[i]>=max)
+			max=bunpu[i];
+printf("%3d",bunpu[i]);	
 }
+	for (j=max;j>=1;j--)
+	{
+		for (i=0;i<num;i++)
+		{
+			if (bunpu[i]==j)
+				printf(" * ");
+			else
+				printf("   ");
+		}
+	}
 	return 0;
 }
