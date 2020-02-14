7b7b88c5cf39fffb2cbce61b566c58691287d66e
hehe <foggyglass@163.com>
2020-02-14 20:44:55 +0800

    add 5-09.c

--- Primer/05.数组/5-09.c
+++ Primer/05.数组/5-09.c
@@ -1,55 +1,54 @@
-#include<stdio.h>
+/*
+	输入学生的分数并显示出分布情况
+*/
 
-#define NUMBER 80
+#include <stdio.h>
+
+#define NUMBER	80		/* 人数上限 */
 
 int main(void)
 {
-    int i, j, max;
-    int num;
-    int tensu[NUMBER];
-    int bunpu[11] = {0};
+	int i, j;
+	int num;				/* 实际的人数 */
+	int tensu[NUMBER];		/* 学生的分数 */
+	int bunpu[11] = {0};	/* 分布图 */
 
-    printf("请输入学生人数: ");
-    do{
-        scanf("%d", &num);
-        if (num < 1 || num > NUMBER)
-            printf("请输入1-%d的数: ", NUMBER);
-    }while (num < 1 || num > NUMBER);
-    printf("请输入%d人的分数。\n", num);
-    for (i = 0; i < num; i++)
-    {
-        printf("%2d号: ", i + 1);
-        do{
-            scanf("%d", &tensu[i]);
-            if (tensu[i] < 1 || tensu[i] > 100)
-                printf("请输入1~100的数: ");
-        }while (tensu[i] < 1 || tensu[i] > 100);
-        bunpu[tensu[i] / 10]++;
-    }
-    puts("\n---分布图---");
-    max = bunpu[0];
-    for (i = 1; i < 11; i++)
-    {
-        if (max < bunpu[i])
-            max = bunpu[i];
-    }
-    for (j = max; j > 0; j--)
-    {
-        for (i = 0; i < 11; i++)
-        {
-            if (bunpu[i] >= j)
-                printf(" *  ");
-            else
-                printf("    ");
-        }
-        putchar('\n');
-    }
-    putchar('\n');
-    printf("---------------------------------------------\n");
-    for (j = 0; j <= 100; j += 10)
-    {
-        printf(" %d ", j);
-    }
+	printf("请输入学生人数：");
 
-    return 0;
-} 
+	do {
+		scanf("%d", &num);
+		if (num < 1 || num > NUMBER)
+			printf("\a 请输入1~%d 的数：", NUMBER);
+	} while (num < 1 || num > NUMBER);
+
+	printf("请输入%d 人的分数。\n", num);
+
+	for (i = 0; i < num; i++) {
+		printf("%2d号：", i + 1);
+		do {
+			scanf("%d", &tensu[i]);
+			if (tensu[i] < 0 || tensu[i] > 100)
+				printf("\a请输入1~100 的数：");
+		} while (tensu[i] < 0 || tensu[i] > 100);
+		bunpu[tensu[i] / 10]++;
+	}
+
+	puts("\n---分布图---");
+
+
+	for (i =0; i <= 9; i++) {
+printf("%d",bunpu[i]);				/* 不到100分 */
+		/*printf("%3d - %3d：", i * 10, i * 10 + 9);
+		for (j = 0; j < bunpu[i]; j++)
+			putchar('*');
+		putchar('\n');
+	}
+	printf("      100：");
+
+	for (j = 0; j < bunpu[10]; j++)			/* 100分 */
+		putchar('*');
+	putchar('\n');*/
+
+}
+	return 0;
+}
