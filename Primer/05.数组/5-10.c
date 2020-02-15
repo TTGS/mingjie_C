0c0ab71cf2ebbacf9750010ed15b785f7f3ef2e6
hehe <foggyglass@163.com>
2020-02-15 20:40:42 +0800

    add 5-10.c

--- Primer/05.数组/5-10.c
+++ Primer/05.数组/5-10.c
@@ -8,7 +8,7 @@ int main(void)
     int  n[4][4]={0};
     int  i,j;
 
-
+/*
 for(i=0;i<=4;i++){
 for(j=0;j<=4;j++){
 printf("a:[%d][%d]:%d",i,j,a[i][j]) ;
@@ -17,11 +17,11 @@ printf("a:[%d][%d]:%d",i,j,n[i][j]) ;
 }
 printf("\n");
 }
+*/
 
-/*
-for(i=0;i<=4;i++){
-for(j=0;j<=4;j++){
-n[i][j]=n[i][j]+a[i][j]*b[i][j];
+for(i=0;i<=2;i++){
+for(j=0;j<=3;j++){
+n[i][j]=n[i][j]+a[i][j]*b[j][i];
 }
 }
 
@@ -31,6 +31,6 @@ printf("%d",n[i][j]) ;
 }
 printf("\n");
 }
-*/
+
     return 0;
 } 
