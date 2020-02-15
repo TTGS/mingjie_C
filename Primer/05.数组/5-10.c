7f09d6f0b2967fb4a64eceef2f897847303b1834
hehe <foggyglass@163.com>
2020-02-15 20:03:09 +0800

    add 5-10.c

---
+++ Primer/05.数组/5-10.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main(void)
+{
+
+    int  a[4][3]={{1,1,1},{2,2,2},{3,3,3},{4,4,4}};
+    int  b[3][4]={{1,1,1,1},{2,2,2,2},{3,3,3,3}};
+    int  n[4][4]={};
+    int  i,j;
+
+for(i=0,i<=4,i++){
+for(j=0,j<=4,j++){
+n[i][j]=n[i][j]+a[i][j]b[i][j];
+}
+}
+
+for(i=0,i<=4,i++){
+for(j=0,j<=4,j++){
+printf("%d",n[i][j]) ;
+}
+printf("\n");
+}
+    return 0;
+} 
