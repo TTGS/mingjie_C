8fd1dfa10ec0ec45573d04c20164c53e23537f42
hehe <foggyglass@163.com>
2020-02-17 21:59:00 +0800

    add 5-10.c

--- Primer/05.数组/5-10.c
+++ Primer/05.数组/5-10.c
@@ -6,36 +6,29 @@ int main(void)
     int  a[4][3]={{1,1,1},{2,2,2},{3,3,3},{4,4,4}};
     int  b[3][4]={{1,1,1,1},{2,2,2,2},{3,3,3,3}};
     int  n[4][4]={0};
-    int  i,j,m,temp;
+    int  i,j,m,h,temp;
 //  6, 6, 6, 6
 // 12,12,12,12
 // 18,18,18,18
 // 24,24,24,24
 
-/*
-for(i=0;i<=4;i++){
-for(j=0;j<=4;j++){
-printf("a:[%d][%d]:%d",i,j,a[i][j]) ;
-printf("b:[%d][%d]:%d",i,j,b[i][j]) ;
-printf("n:[%d][%d]:%d",i,j,n[i][j]) ;
-}
-printf("\n");
-}
-*/
-
 m=0;
 temp=0;
+h=0;
 
 	temp=0;
-	for(i=0;i<=3;i++){
-	temp=0;
-		for(j=0;j<=2;j++){
-			temp=temp+a[i][j]*b[j][i];
-			printf("i=%d , j=%d, m=%d,h=%d,temp=%d\n",i,j,m,h,temp) ;
-		}
-	n[m][i]=temp;
-	if (i==3) 
-		m++;
+	for(i=0;i<=3;i++)
+	{
+		temp=0;
+			for(j=0;j<=2;j++)
+			{
+				temp=temp+a[i][j]*b[j][i];
+				printf("i=%d , j=%d, m=%d,h=%d,temp=%d\n",i,j,m,h,temp) ;
+			}
+		n[m][h]=temp;
+		if (i==3) 
+			m++;
+		h++;
 
 	}
 
