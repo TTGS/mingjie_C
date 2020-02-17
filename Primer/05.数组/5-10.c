ffbae9eabaf6aae1d8783a83d812f1c6759fe177
hehe <foggyglass@163.com>
2020-02-17 21:35:59 +0800

    add 5-10.c

--- Primer/05.数组/5-10.c
+++ Primer/05.数组/5-10.c
@@ -6,7 +6,7 @@ int main(void)
     int  a[4][3]={{1,1,1},{2,2,2},{3,3,3},{4,4,4}};
     int  b[3][4]={{1,1,1,1},{2,2,2,2},{3,3,3,3}};
     int  n[4][4]={0};
-    int  i,j,k,p,l,temp[4]={0};
+    int  i,j,temp;
 
 /*
 for(i=0;i<=4;i++){
@@ -18,29 +18,22 @@ printf("n:[%d][%d]:%d",i,j,n[i][j]) ;
 printf("\n");
 }
 */
-p=0;
-for (k=0;k<=3;k++){
+
+temp=0;
 for(i=0;i<=2;i++){
 for(j=0;j<=3;j++){
-temp[i]=temp[i]+a[i][j]*b[j][i];
-printf("temp:[%d]:%d\n",i,temp[i]) ;
+temp=temp+a[j][i]*b[i][j];
+printf("i=%d , j=%d ,temp=%d\n"i,j,temp) ;
 }
-
-for(l=0;l<=4;l++)
-{
-n[p][l]=temp[l];
 }
-p++;
-}
-temp[4]={0};
-}
-
+//显示 矩阵乘法之后的内容。
 for(i=0;i<=3;i++){
 for(j=0;j<=3;j++){
 printf("%5d",n[i][j]) ;
 }
 printf("\n");
 }
+
 
     return 0;
 } 
