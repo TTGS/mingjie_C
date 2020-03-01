7fa556a1d21b88cdad0d853934ef55c010d2461e
hehe <foggyglass@163.com>
2020-03-01 22:14:00 +0800

    add 6-10.c

---
+++ Primer/06.函数/6-10.c
@@ -0,0 +1,26 @@
+ #include <stdio.h>
+
+int intary_rcpy(int v1[] , const int v2[] , int n)
+{
+int i ;
+	for(i=0;i<n;i++)
+	{
+		 v1[i]=v2[n-i-1];
+	} ;
+}
+
+
+int main(void)
+{
+	int j;
+	int k[5]={1,2,3,4,5};
+	int q[5]={0};
+	printf("数组倒序为：\n");
+ 	intary_rcpy(q,k,5);
+	for(j=0;j<5;j++)
+	{
+	printf(" %d,%d " ,j,q[j]);
+	}
+	printf("\n");
+	return 0;
+} 
