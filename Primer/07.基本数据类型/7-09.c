af794e182da34f0d32ceb893167ce16a22023206
hehe <foggyglass@163.com>
2020-03-11 21:34:55 +0800

    add 7-09.c

---
+++ Primer/07.基本数据类型/7-09.c
@@ -0,0 +1,14 @@
+#include <math.h>
+#include <stdio.h>
+  
+int main (void)
+{
+	double x;
+	
+	printf("请输入正方形面积大小：");
+	scanf("%lf",&x);
+
+	printf("该正方形的 边长为：%f\n",
+                sqrt(x));
+	return 0; 
+ }  
