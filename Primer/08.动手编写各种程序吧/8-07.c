ec68a0f886b7a0439e6e539f631dc67b40f55132
hehe <foggyglass@163.com>
2020-03-15 20:18:44 +0800

    add 8-07.c

---
+++ Primer/08.动手编写各种程序吧/8-07.c
@@ -0,0 +1,34 @@
+ 
+#include <stdio.h>
+
+/*--- 返回组合的值 ---*/
+int combination(int n,int r)
+{
+int i,c=1,m=1; 
+	for(i=1;i<=r;i++)
+	{
+		m=m*i;
+	}
+
+	for(i=r;i<=n;i++)
+	{
+		c=c*i;
+	}
+
+return c/m;
+}
+
+int main(void)
+{
+	int n,r;
+
+	printf("请输入n个整数中取出r个整数的组合数：\n");
+printf("请输入n：");
+scanf("%d", &n);
+printf("请输入r：");
+scanf("%d", &r);
+
+	printf("%d个整数中取出%d个整数的组合数:%d \n", n ,r, combination(n,r));
+
+	return 0;
+}
