8c3f7bba40fe63239202e4ffe8566edcff3f0efe
hehe <foggyglass@163.com>
2020-03-15 19:38:34 +0800

    add 8-05.c

---
+++ Primer/08.动手编写各种程序吧/8-05.c
@@ -0,0 +1,23 @@
+/*
+	读取学生的身高并排序
+*/
+
+#include <stdio.h>
+
+//性别
+enum xb {female,male};
+//四季
+enum jj {spring,summer,fall,winter};
+ 
+
+int main(void)
+{
+	enum xb xbn;
+	enum jj jjn;
+
+	 
+	printf(" male为%d\n female为%d\n spring为%d\n summer为%d\n fall为%d\n winter为%d\n"
+               ,male      ,female     ,spring      ,summer      ,fall,     winter);
+	 
+	return 0;
+}
