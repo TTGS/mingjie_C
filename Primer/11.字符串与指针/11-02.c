0ba4c83dd627a92fdd09c45e9feca0910d86f77a
hehe <foggyglass@163.com>
2020-03-22 20:07:45 +0800

    add 11-02.c

---
+++ Primer/11.字符串与指针/11-02.c
@@ -0,0 +1,19 @@
+ 
+
+#include <stdio.h>
+
+int main(void)
+{
+	int  i =0 ;
+	char a[][5] = {"LISP", "C", "Ada"};
+	char *p[]   = {"PAUL", "X", "MAC"};
+
+	for (i = 0;  i<  (sizeof(a) / sizeof(a[0])); i++)
+		printf("a[%d] = \"%s\"\n", i, a[i] );
+
+ 
+	for (i = 0; i <  (sizeof(p) / sizeof(p[0])); i++)
+		printf("p[%d] = \"%s\"\n", i, p[i]);
+
+	return 0;
+} 
