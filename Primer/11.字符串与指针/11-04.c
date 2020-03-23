b835e60cd31ad335224e9632452efd55bac83443
hehe <foggyglass@163.com>
2020-03-23 15:10:21 +0800

    add 11-04.c

---
+++ Primer/11.字符串与指针/11-04.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*--- 将字符串s复制到d ---*/
+void put_string(const char *s)
+{
+putchar( *s); 
+	while (  *s++)
+		putchar(*s); 
+}
+
+int main(void)
+{
+	char str[128] ; 
+ 
+	printf("复制的是：" );
+	scanf("%s", str);
+
+	put_string(str);
+	return 0;
+}
